c9f8153f50f4a9b4be1def012f92f58d263f90cd
windsuzu <windsuzu@gmail.com>
2022-02-20 22:25:23 +0800

    fix gh-pages routing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    // Fix the routing of gh-pages by adding basename
+    // https://medium.com/@Dragonza/react-router-problem-with-gh-pages-c93a5e243819
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
     </BrowserRouter>,
     document.getElementById("root")
